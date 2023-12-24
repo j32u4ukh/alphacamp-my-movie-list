10e0cd55e0c27ebd3d2e0425fa0193817ddc8494
j32u4ukh <j32u4ukh@gmail.com>
2023-12-24 09:47:28 +0800

    feat: 透過 icon 在「清單模式」與「卡片模式」間切換

--- index.js
+++ index.js
@@ -3,11 +3,18 @@ const API_URL = `${BASE_URL}/api/movies`;
 const POSTER_URL = `${BASE_URL}/posters`;
 const MOVIES_PER_PAGE = 12;
 const movies = [];
-let currentMovies = [];
+const icons = document.querySelector(".icons");
 const dataPanel = document.querySelector("#data-panel");
 const searchForm = document.querySelector("#search-form");
 const searchInput = document.querySelector("#serach-input");
 const paginator = document.querySelector("#paginator");
+const DisplayMode = {
+  Block: "Block",
+  List: "List",
+};
+let currentMovies = [];
+let currentPage = 1;
+let displayMode = DisplayMode.Block;
 
 function getMoviesByPage(movies, page) {
   const start = MOVIES_PER_PAGE * (page - 1);
@@ -16,14 +23,26 @@ function getMoviesByPage(movies, page) {
 
 function renderMovies(movies) {
   dataPanel.innerHTML = "";
-  movies.forEach(renderMovie);
+  let parent;
+  if (displayMode === DisplayMode.Block) {
+    parent = dataPanel;
+  } else {
+    parent = document.createElement("ul");
+    parent.classList.add("list-group", "col-sm-12", "mb-2");
+    dataPanel.appendChild(parent);
+  }
+  movies.forEach((movie) => {
+    renderMovie(parent, movie);
+  });
 }
 
-function renderMovie(data) {
+function renderMovie(parent, data) {
   let src = `${POSTER_URL}/${data.image}`;
-  let div = document.createElement("div");
-  div.classList.add("col-sm-3");
-  div.innerHTML = `<div class="mb-2">
+  let child;
+  if (displayMode === DisplayMode.Block) {
+    child = document.createElement("div");
+    child.classList.add("col-sm-3");
+    child.innerHTML = `<div class="mb-2">
       <div class="card">
         <img src="${src}" class="card-img-top" alt="Movie Poster">
         <div class="card-body">
@@ -35,7 +54,16 @@ function renderMovie(data) {
         </div>
       </div>
     </div>`;
-  dataPanel.appendChild(div);
+  } else {
+    child = document.createElement("li");
+    child.classList.add("list-group-item", "d-flex", "justify-content-between");
+    child.innerHTML = `<h5 class="card-title">${data.title}</h5>
+                    <div>
+                        <button class="btn btn-primary btn-show-movie" data-bs-toggle="modal" data-bs-target="#movie-modal" data-id="${data.id}">More</button>
+                        <button class="btn btn-info btn-add-favorite" data-id="${data.id}">+</button>
+                    </div>`;
+  }
+  parent.appendChild(child);
 }
 
 function showMovieModal(id) {
@@ -54,9 +82,10 @@ function showMovieModal(id) {
 
 function addToFavorite(id) {
   const favorites = JSON.parse(localStorage.getItem("favorite-movies")) ?? [];
-  console.log(`favorites: ${favorites}`);
   const movie = movies.find((m) => m.id === id);
-  if (!favorites.some((m) => m.id === id)) {
+  if (favorites.some((m) => m.id === id)) {
+    alert(`電影 '${movie.title}' 已收藏`);
+  } else {
     favorites.push(movie);
     localStorage.setItem("favorite-movies", JSON.stringify(favorites));
   }
@@ -91,7 +120,8 @@ function init() {
 
     currentMovies = currentMovies.length === 0 ? movies : currentMovies;
     setPaginator(currentMovies.length);
-    renderMovies(getMoviesByPage(currentMovies, 1));
+    currentPage = 1;
+    renderMovies(getMoviesByPage(currentMovies, currentPage));
   });
 
   paginator.addEventListener("click", function onPageSelected(event) {
@@ -100,11 +130,26 @@ function init() {
 
     if (target.matches(".page-item")) {
       let a = target.children[0];
-      let page = Number(a.innerHTML);
-      renderMovies(getMoviesByPage(currentMovies, page));
+      currentPage = Number(a.innerHTML);
+      renderMovies(getMoviesByPage(currentMovies, currentPage));
     } else if (target.matches(".page-link")) {
-      let page = Number(target.innerHTML);
-      renderMovies(getMoviesByPage(currentMovies, page));
+      currentPage = Number(target.innerHTML);
+      renderMovies(getMoviesByPage(currentMovies, currentPage));
+    }
+  });
+
+  icons.addEventListener("click", function onIconClicked(event) {
+    let target = event.target;
+    let needRender = false;
+    if (target.matches(".fa-bars") && displayMode !== DisplayMode.List) {
+      needRender = true;
+      displayMode = DisplayMode.List;
+    } else if (target.matches(".fa-th") && displayMode !== DisplayMode.Block) {
+      needRender = true;
+      displayMode = DisplayMode.Block;
+    }
+    if (needRender) {
+      renderMovies(getMoviesByPage(currentMovies, currentPage));
     }
   });
 
@@ -115,7 +160,8 @@ function init() {
       movies.push(...results);
       currentMovies = movies;
       setPaginator(movies.length);
-      renderMovies(getMoviesByPage(movies, 1));
+      currentPage = 1;
+      renderMovies(getMoviesByPage(movies, currentPage));
     })
     .catch((error) => {
       console.log(error);
